a7343e0a53fe4dade4addc6f3f5cac57523915fe
NEHA MISHRA <81455789+Neha993@users.noreply.github.com>
2022-08-04 10:25:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//components
+import Connection from './database/db.js';
+import Router from './routes/route.js';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Router);
+
+
+const PORT = 8000;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
